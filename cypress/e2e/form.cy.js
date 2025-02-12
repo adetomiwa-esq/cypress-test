0e1e1e6e2fdab849e70ffbd3cdb1e04ccbbf27c8
Akinola Kehinde <khennyakin@gmail.com>
2025-02-12 16:01:47 +0100

    Data driven framework in automation testing

--- cypress/e2e/form.cy.js
+++ cypress/e2e/form.cy.js
@@ -1,8 +1,12 @@
 describe("template spec", () => {
   it("passes", () => {
     cy.visit("http://localhost:5173/");
-    cy.get("#email").type("codar@gmail.com");
 
-    cy.get("#btn").click();
+    cy.fixture("data.json").then((data) => {
+      cy.get("#email").type(data.users[1].email);
+    });
+    //
+
+    // cy.get("#btn").click();
   });
 });
